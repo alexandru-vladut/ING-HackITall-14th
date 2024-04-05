d6214a6476dce9d9d3ac90b653a7b8b2a0657f8b
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2024-04-05 22:08:21 +0300

    Connected to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ing_mobile/features/authentication/start_checks.dart';
 import 'package:ing_mobile/main_provider.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         return ChangeNotifierProvider<MainProvider>(
           create: (context) => MainProvider(),
           builder: (context, child) => const MaterialApp(
-            title: "IP-Banking-Mobile",
+            title: "ing_mobile",
             debugShowMaterialGrid: false,
             debugShowCheckedModeBanner: false,
             home: CheckLoginStatus(),
