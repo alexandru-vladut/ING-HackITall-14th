075187bc4782ed51b9bdaf28295cdf9b5f572609
bdykdz <129845958+bdykdz@users.noreply.github.com>
2024-04-06 14:42:21 +0300

    added data to products

--- lib/configs/products.dart
+++ lib/configs/products.dart
@@ -1,6 +1,46 @@
-Map<String, Map<String, String>> products = {
+Map<String, Map<String, dynamic>> products = {
     '59489184': {
-      'key1': 'value1',
-      'key2': 'value2',
+      'name': 'Pack of Heets',
+      'redgreen': 'red',
+      'paper': 0.05,
+      'foil': 0,
+      'plastic': 0,
+      'tobacco': 0,
+      'points': 1,
+      'CO2e' : 2,
+      'Water Waste': 50,
+      'Disposal Recommendations': 'Dispose of packaging in general waste. Tobacco residue should be treated as hazardous waste due to chemicals.'
+      
     },
+
+    '5948918': {
+      'name': 'Coca Cola Bottle',
+      'redgreen': 'green',
+      'paper': 0.05,
+      'foil': 0.05,
+      'plastic': 0.01,
+      'points': 5,
+      'CO2e' : 0.4,
+      'Water Waste': 2,
+      'Disposal Recommendations': 'Rinse and recycle in a plastic recycling bin. Cap and label can be left on.'
+    },
+
+    '59489181': {
+      'name': 'Ipad',
+      'redgreen': 'green',
+      'cardboard': 0.3,
+      'gold': 0.05,
+      'plastic': 0.15,
+      'silver': 0.01,
+      'copper': 0.2,
+      'aluminium': 0.4,
+      'glass': 0.2,
+      'lithium-ion': 0.2,
+      'points': 3500,
+      'CO2e' : 150,
+      'Disposal Recommendations': 'Do not dispose of in general waste. Use an electronics recycling program or return to an electronics retailer for proper handling.'
+
+    },
+
+
   };
